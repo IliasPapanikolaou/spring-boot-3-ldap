5bea3eca99d1710a627fd82b1daf0d246d3db275
Ilias Papanikolaou <eliaspapanikolaou@hotmail.com>
2023-11-18 18:14:00 +0200

    java 17/sb3.1.5 impl

--- src/main/java/com/ipap/springboot3ldap/SecurityConfiguration.java
+++ src/main/java/com/ipap/springboot3ldap/SecurityConfiguration.java
@@ -1,24 +1,27 @@
 package com.ipap.springboot3ldap;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.LdapShaPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
-@EnableWebSecurity
-public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+@Configuration
+public class SecurityConfiguration {
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http
-                .authorizeRequests()
-                .anyRequest().fullyAuthenticated()
-                .and()
-                .formLogin();
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .authorizeHttpRequests(auth -> auth.anyRequest().fullyAuthenticated())
+                .formLogin(Customizer.withDefaults())
+                .build();
     }
 
-    @Override
+    @Autowired
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.ldapAuthentication()
                 // DN: Distinguish Name: the way user information is stored in ldif
@@ -30,7 +33,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .url("ldap://localhost:8389/dc=springframework,dc=org")
                 .and()
                 .passwordCompare()
-                .passwordEncoder(new LdapShaPasswordEncoder())
+                .passwordEncoder(new LdapShaPasswordEncoder()) // Use BCrypt instead
                 .passwordAttribute("userPassword");
     }
 }
