097fbc24d3e09bae3b06dcd384205afb7b6992b7
Ilias Papanikolaou <eliaspapanikolaou@hotmail.com>
2023-11-18 17:43:36 +0200

    init commit

---
+++ src/main/java/com/ipap/springboot3ldap/SpringBoot3LdapApplication.java
@@ -0,0 +1,13 @@
+package com.ipap.springboot3ldap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot3LdapApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBoot3LdapApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ipap/springboot3ldap/SpringBoot3LdapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ipap.springboot3ldap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBoot3LdapApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
